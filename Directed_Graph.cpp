66e6f67dd8b26771a01bd2944bc060b1c10f2cd2
PRASANJIT ROY <roy2402011@gmail.com>
2026-02-19 11:01:28 +0600

    by me

---
+++ Directed_Graph.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int n,e;
+     cin>>n>>e;
+     int adj_mat[n][e];
+     for(int i=0;i<e;i++)
+     {
+       for(int j=0;j<n;j++)
+       {
+        adj_mat[i][j]=0;
+
+       }
+       }
+
+    memset(adj_mat,0,sizeof(adj_mat));
+
+      for(int i=0;i<e;i++)
+     {
+        int a,b;
+        cin>>a>>b;
+        adj_mat[a][b]=1;
+        adj_mat[b][a]=1;
+
+
+     }
+
+       for(int i=0;i<e;i++)
+     {
+       for(int j=0;j<n;j++)
+       {
+        cout<<adj_mat[i][j]<<" ";
+       }
+       cout<<"\n";
+
+     }
+
+
+    return 0;
+}
