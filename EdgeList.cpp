bdec41c369faf11060b81a227b3147042cdd3e81
PRASANJIT ROY <roy2402011@gmail.com>
2026-02-20 15:23:17 +0600

    by me

---
+++ EdgeList.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int n,e;
+     cin>>n>>e;
+     vector<pair<int,int>>edge_list;
+     while(e--){
+        int a,b;
+        cin>>a>>b;
+        edge_list.push_back({a,b});
+
+     }
+     
+
+    return 0;
+}
