f738d6b0fa9bebc09f5d5edfe415227e4fe4eaac
PRASANJIT ROY <roy2402011@gmail.com>
2026-02-24 23:20:17 +0600

    by me

---
+++ connectedOrNOt.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     int n,e;
+     cin>>n>>e;
+     int adj_mat[n][e];
+     for(int i=0;i<e;i++)
+     {
+       for(int j=0;j<n;j++)
+       {
+        adj_mat[i][j]=0;
+         if(i==j){
+        adj_mat[i][j]=1;
+       }
+
+       }
+      
+
+
+     }
+
+     memset(adj_mat,0,sizeof(adj_mat));
+
+      for(int i=0;i<e;i++)
+     {
+        int a,b;
+        cin>>a>>b;
+        adj_mat[a][b]=1;
+        adj_mat[b][a]=1;
+        
+
+
+     }
+
+      int t;
+      cin>>t;
+      while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(adj_mat[a][b])
+        {
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+      }
+
+    return 0;
+}
